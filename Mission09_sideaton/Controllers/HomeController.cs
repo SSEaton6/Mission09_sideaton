b930dcaf50840ef0b99b93d5fa3e4a779afd4d73
Sidney Eaton <sideaton@byu.edu>
2023-03-01 23:01:11 -0700

    midway through update

---
+++ Mission09_sideaton/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_sideaton.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_sideaton.Controllers
+{
+    public class HomeController : Controller
+    {
+        //instead of going directly through the context, we use a repository to protect the data.
+        private IBookstoreRepository repo;
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        
+        public IActionResult Index()
+        {
+            var stuff = repo.Books.ToList();
+            return View(stuff);
+        }
+    }
+}
